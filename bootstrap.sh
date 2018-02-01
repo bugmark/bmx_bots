9c76f151ff130957da91ba22373a744d7e9349bb
andyl <andy@r210.com>
2018-02-01 09:15:05 -0800

    Tweak wording

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ gem specific_install http://github.com/bugmark/bmx_api_ruby
 gem specific_install http://github.com/bugmark/bmx_cl_ruby
 
 echo "Create configuration for localhost"
+echo "(You'll have to change the hostname if you're using a remote server...)"
 bmx config set --scheme=https --host=localhost --usermail=admin@bugmark.net --password=bugmark 
 
 echo "Show Bugmark host info"
