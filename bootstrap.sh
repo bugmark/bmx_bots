f590be1fe359d8430aa2e886cd7c1d171f832788
andyl <andy@r210.com>
2018-02-01 12:47:58 -0800

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,10 @@
 echo "Download gem installer"
 gem install specific_install
 
+echo "Remove old gems"
+gem uninstall bmx_api_ruby
+gem uninstall bmx_cl_ruby 
+
 echo "Install/Upgrade Bugmark CLI"
 gem specific_install http://github.com/bugmark/bmx_api_ruby
 gem specific_install http://github.com/bugmark/bmx_cl_ruby
@@ -11,8 +15,4 @@ echo "Create configuration for localhost"
 echo "(You'll have to change the hostname if you're using a remote server...)"
 bmx config set --scheme=https --host=localhost --usermail=admin@bugmark.net --password=bugmark 
 
-echo "Show Bugmark host info"
-bmx host info
-
-echo "Show database counts"
-bmx host counts
+bmx help
