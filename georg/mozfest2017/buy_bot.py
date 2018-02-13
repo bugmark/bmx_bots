8086e26f49318e0066e469480e01e8fc467f3568
Georg Link <linkgeorg@gmail.com>
2018-02-12 18:25:42 -0600

    finish buy_bot with api calls

--- georg/mozfest2017/buy_bot.py
+++ georg/mozfest2017/buy_bot.py
@@ -22,24 +22,24 @@ import time
 import datetime
 
 # possible maturation dates: array of dates determine by ???
-maturations = []
+maturations = json.loads(check_output(["bmx", "host", "next_week_ends",
+                                      "--strftime=%Y%m%d_%H%M"])
+                         .decode("utf-8"))
 # possible users: manually enter array of users and passwords
-users = ["test"+str(x)+"@bugmark.net" for x in range(1,  5)]
+users_obj = json.loads(check_output(["bmx", "user", "list",
+                                    "--with_email=test"])
+                       .decode("utf-8"))
+users = [x['email'] for x in users_obj]
 password = "bugmark"
-# possible issues: arrary of  first four issues from Bugmark
-issues = []
-# access the bugmark exchange and get list of issues
-issue_list = check_output(["bmx", "issue", "list"])
-# extract first four issues from the list of issues
-json_il = json.loads(issue_list.decode("utf-8"))
-for p in json_il:
-    issues.append(p['uuid'])
-    if(len(issues) > 3):
-        break
+# possible issues: arrary of first three issues from Bugmark
+issues_obj = json.loads(check_output(["bmx", "issue", "list",
+                                     "--limit=3"])
+                        .decode("utf-8"))
+issues = [x['uuid'] for x in issues_obj]
 # pssible volumes: array of increments of 5 from 30 to 50
 volumes = [x*5 for x in range(6, 11)]
 # possible prices: array of increments of .05 from .05 to 0.95
-prices = [x*.05 for x in range(1, 20)]
+prices = [round(x*.05, 2) for x in range(1, 20)]
 # possible sides: array with values 'fixed' and 'unfixed'
 sides = ['fixed', 'unfixed']
 
@@ -48,14 +48,18 @@ sides = ['fixed', 'unfixed']
 # random combination of user, issue, volume, price, and side
 def buy():
     secure_random = random.SystemRandom()
-    new_offer = check_output(["bmx", "offer", "buy",
+    new_offer = check_output(["bmx", "offer", "create_buy",
                               "--side="+secure_random.choice(sides),
                               "--volume="+secure_random.choice(volumes),
                               "--price="+secure_random.choice(prices),
                               "--issue="+secure_random.choice(issues),
                               "--maturation=" +
-                              secure_random.choice(maturations)])
-    new_contract = check_output(["bmx", "offer", "cross", new_offer["uuid"]])
+                              secure_random.choice(maturations),
+                              "--userspec="+secure_random.choice(users) +
+                              ":"+password])
+    new_offer_uuid = json.loads(new_offer.decode("utf-8"))["uuid"]
+    check_output(["bmx", "offer", "cross", new_offer_uuid,
+                 "--commit-type=expand"])
 
 
 # output
@@ -68,17 +72,20 @@ print("Loading Environment...")
 for x in range(1, 100):
     ttime = datetime.time()
     buy()
-    open_offers = check_output(["bmx", "offer", "list"])
-    contracts = check_output(["bmx", "contract", "list"])
-    escrows = check_output(["bmx", "escrow", "list"])
+    open_offers = json.loads(check_output(["bmx", "offer", "list"])
+                             .decode("utf-8"))
+    contracts = json.loads(check_output(["bmx", "contract", "list"])
+                           .decode("utf-8"))
+    escrows = json.loads(check_output(["bmx", "escrow", "list"])
+                         .decode("utf-8"))
     print("Cycle: "+str(x)+" | " +
           ttime.strftime("%H:%M:%S")+" | " +
           len(open_offers)+" open offers | " +
           len(contracts)+" contracts | " +
           len(escrows)+" escrows")
     if x < 50:
-        time.sleep(5)
+        # time.sleep(5)
     else:
-        time.sleep(20)
+        # time.sleep(20)
 ttime = datetime.time()
 print("Terminating after 99 cycles "+ttime.strftime("%H:%M:%S"))
