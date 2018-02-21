96a5562321f86bd317104f75617e5290ad3b07d5
Georg Link <linkgeorg@gmail.com>
2018-02-21 15:46:57 -0600

    minor change

--- georg/trivial_case_1/person.py
+++ georg/trivial_case_1/person.py
@@ -99,12 +99,13 @@ class PTrivialCase1Worker(Person):
         offer = json.loads(offer_rtn.decode("utf-8"))
         # do the work and close the issue
         # get repo uuid to be able to update issue
-        issue_rtn = check_output(["bmx", "issue", "show", str(issue)])
-        issue_obj = json.loads(issue_rtn.decode("utf-8"))
+        # issue_rtn = check_output(["bmx", "issue", "show", str(issue)])
+        # issue_obj = json.loads(issue_rtn.decode("utf-8"))
         check_output(["bmx", "issue", "sync",
                       str(issue),
-                      "--status=closed",
-                      "--repo_uuid="+issue_obj["stm_repo_uuid"]])
+                      "--status=closed"
+                      # "--repo_uuid="+issue_obj["stm_repo_uuid"]
+                      ])
         # check_output(["bmx", "contract", "list"])
         #    return 1
         return offer
